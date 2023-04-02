d100a47a2c94e7dde2e22d35c2128d3fdacddc59
MaksimCh1988 <maksim.ch@yahoo.com>
2023-04-02 18:27:14 +0300

    3

--- siteblog/blog/views.py
+++ siteblog/blog/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
 
 def index(request):
-    return HttpResponse('Hello world!')
+    return render(request, 'blog/index.html')
 
-# Create your views here.
+
--- siteblog/siteblog/settings.py
+++ siteblog/siteblog/settings.py
@@ -56,7 +56,9 @@ ROOT_URLCONF = 'siteblog.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            BASE_DIR / 'templates' ,
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -118,6 +120,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'static'
+STATICFILES_DIRS = [
+    BASE_DIR / 'siteblog/static',
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
